f15a6088bddf4d2e993a6280d11d824e52d4249c
Karl Tayeb <karl.tayeb7@gmail.com>
2023-09-06 15:49:23 -0400

    generalize away from logistic susie, using `logreg` interface

--- R/como2.R
+++ R/como2.R
@@ -9,59 +9,57 @@ prep_data_como2 <- function(betahat, se, X, Z){
   return(data)
 }
 
-compute_post_assignment <- function(fit) {
+compute_post_assignment <- function(fit, data) {
   # ln p(z=1)/p(z=0)
-  logit_pi <- compute_Xb.binsusie(fit)
-  f0_loglik <- convolved_logpdf(fit$f0, fit$data$betahat, fit$data$se)
-  f1_loglik <- convolved_logpdf(fit$f1, fit$data$betahat, fit$data$se)
-  logits <- f1_loglik - f0_loglik + logit_pi
+  prior_log_odds <- compute_prior_log_odds(fit$logreg, data)
+  f0_loglik <- convolved_logpdf(fit$f0, data$betahat, data$se)
+  f1_loglik <- convolved_logpdf(fit$f1, data$betahat, data$se)
+  logits <- (f1_loglik - f0_loglik) + prior_log_odds
   post_assignment <- sigmoid(logits)
   return(post_assignment)
 }
 
-compute_assignment_entropy <- function(fit, data) {
-  p <- data$y
-  entropy <- -1 * (p * log(p) + (1 - p) * log(1 - p))
-  return(entropy)
+compute_assignment_entropy <- function(p) {
+  q <- 1 - p
+  h <- -1 * ((p * log(p)  + q * log(q)))
+  return(sum(h, na.rm = T))
 }
 
 #' E[logp(y | f0, f1, z)]
-#'
+#' @export
 loglik.como2 <- function(fit, data) {
   f0_loglik <- fit$f0_loglik # convolved_logpdf(fit$f0, fit$data$betahat, fit$data$se)
   f1_loglik <- fit$f1_loglik # convolved_logpdf(fit$f1, fit$data$betahat, fit$data$se)
-  y <- data$y
-  loglik <- (1 - y) * f0_loglik + y * f1_loglik
+  q <- compute_post_assignment(fit, data)
+  loglik <- (1 - q) * f0_loglik + q * f1_loglik
   return(loglik)
 }
 
 #' @export
 compute_elbo.como2 <- function(fit, data) {
+  post_assignment <- compute_post_assignment(fit, data)
   data_loglik <- sum(loglik.como2(fit, data))
-  assignment_entropy <- sum(compute_assignment_entropy(fit, data), na.rm = TRUE)
-  logreg_elbo <- tail(fit$logreg$elbo, 1)
+  assignment_entropy <- compute_assignment_entropy(post_assignment)
+  logreg_elbo <- compute_elbo(fit$logreg, post_assignment, data)
   if(is.null(logreg_elbo)){
     logreg_elbo <- -Inf
   }
-  elbo <- data_loglik + assignment_entropy + logreg_elbo
+  elbo <- (data_loglik + assignment_entropy) + logreg_elbo
   return(elbo)
 }
 
 #' @export
 update_model.como2 <- function(fit, data, estimate_f1=FALSE, track_elbo=T){
   # compute posterior assignment
-  fit$logits <- fit$f1_loglik - fit$f0_loglik + fit$psi$mu
-  data$y <- sigmoid(fit$logits)
-
-  # update logreg, store expected predictions in fit$psi$mu
-  fit$logreg <- logisticsusie::update_model(fit$logreg, data, track_elbo=T)
-  psi <- logisticsusie::compute_psi(fit$logreg, data)
-  fit$psi$mu <- psi
+  prior_log_odds <- compute_prior_log_odds(fit$logreg, data)
+  post_log_odds <- (fit$f1_loglik - fit$f0_loglik) + prior_log_odds
+  qz <- sigmoid(post_log_odds)
+  fit$logreg <- update_prior(fit$logreg, qz, data)
 
   if (estimate_f1) {
     fit$f1 <- update_params(
       fit$f1, data$betahat, data$se,
-      weights = data$y
+      weights = qz
     )
     fit$f1_loglik <- convolved_logpdf(fit$f1, data$betahat, data$se)
   }
@@ -73,7 +71,7 @@ update_model.como2 <- function(fit, data, estimate_f1=FALSE, track_elbo=T){
 }
 
 #' @export
-data_initialize_como2 <- function(data, L, mu0=0, var0=1, f1_dist='normal', f1_params = list()){
+data_initialize_como2 <- function(data, mu0=0, var0=1, f1_dist='normal', f1_params = list(), logreg='constant', logreg_params = list()){
 
   # initialize component distribution
   f0 <- point_component(mu = 0)
@@ -89,7 +87,14 @@ data_initialize_como2 <- function(data, L, mu0=0, var0=1, f1_dist='normal', f1_p
   logits <- f1_loglik - f0_loglik
 
   # initialize logreg
-  logreg <- logisticsusie:::data_initialize_binsusie(data, L, mu0, var0)
+  if(logreg == 'constant'){
+    logreg <- rlang::exec(initialize_constant_logreg, !!!logreg_params)
+  }
+
+  if(logreg == 'linear_susie'){
+    logreg_params$n <- length(f0_loglik)
+    logreg <- rlang::exec(initialize_linear_susie, !!!logreg_params)
+  }
 
   fit <-list(
     logreg = logreg,
@@ -97,7 +102,6 @@ data_initialize_como2 <- function(data, L, mu0=0, var0=1, f1_dist='normal', f1_p
     f0_loglik = f0_loglik,
     f1 = f1,
     f1_loglik = f1_loglik,
-    psi = list(mu=0, mu2=0),
     elbo = -Inf
   )
   class(fit) <- 'como2'
